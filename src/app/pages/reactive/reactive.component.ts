f0dfb444caf7ab7ddc6de4e626a52ae0c9651607
Anthony Cruz <ms_anthony_cruz@hotmail.com>
2023-10-06 15:49:34 -0500

    form reactivo-validaciones

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -1,10 +1,58 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
   templateUrl: './reactive.component.html',
-  styleUrls: ['./reactive.component.css']
+  styleUrls: ['./reactive.component.css'],
 })
 export class ReactiveComponent {
+
+  forma: FormGroup = new FormGroup({});
+
+  constructor(private fb: FormBuilder) {
+    this.crearFomulario();
+  }
+
+
+  // Validaciones Formularios reactive
+  get nombreNoValido(){
+    return this.forma.get('nombre')?.invalid && this.forma.get('nombre')?.touched
+  }
+
+  get apellidoNoValido(){
+    return this.forma.get('apellido')?.invalid && this.forma.get('apellido')?.touched
+  }
+
+  get correoNoValido(){
+    return this.forma.get('correo')?.invalid && this.forma.get('correo')?.touched
+  }
+
+  // Funciones de formulario reactive
+  crearFomulario() {
+    this.forma = this.fb.group({
+      nombre: ['', [Validators.required, , Validators.minLength(4)]],
+      apellido: ['', Validators.required],
+      correo: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,3}$'),
+        ]
+      ]
+    });
+  }
+
+  guardar() {
+    console.log(this.forma);
+    if (this.forma.invalid) {
+      return Object.values(this.forma.controls).forEach(control =>{
+        control.markAsTouched();
+      });
+    }
+  }
+
+
+  
 
 }
